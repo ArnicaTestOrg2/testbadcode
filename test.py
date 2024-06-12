3e152b8cec4479e701f7e825174a5570a8b2a41d
Jacob Barnum <70239630+jacobtb2323@users.noreply.github.com>
2024-06-12 08:16:23 -0600

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request
 app = Flask(__name__)
 
-# curl -X GET "http://localhost:5000/tainted7/touch%20HELLO"
+# curl -X GET "http://localhost:5000/tainted7/touch%20HELLO" Lets try this again shall we...
 @app.route("/tainted7/<something>")
 def test_sources_7(something):
     
